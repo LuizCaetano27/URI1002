33e8f39baaad7a5a5ebd2530cd8d4fa1930e62e7
luiz.caetano <luiz.caetano@MMQ003477>
2023-09-21 15:24:44 -0300

    Adicionar arquivos de projeto.

---
+++ URI1002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = (3.14159 * raio * raio);
+
+            Console.WriteLine("A= " + area.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
